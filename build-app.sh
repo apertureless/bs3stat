ba413f7e7903b98e3c272a1d4b4b97961dc3b210
Jakub Juszczak <netghost03@gmail.com>
2016-12-04 14:16:06 +0100

    🐛 Fix  go build command

--- build-app.sh
+++ build-app.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "Building application"
-godep CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bs3stat .
+CGO_ENABLED=0 GOOS=linux godep go build -a -installsuffix cgo -o bs3stat .
 
